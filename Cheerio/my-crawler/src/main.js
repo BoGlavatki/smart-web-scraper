b1827201c9991ea6bddebe1743a04a0ca6a28831
BoGlavatki <82083692+BoGlavatki@users.noreply.github.com>
2026-07-03 16:17:30 +0200

    refactor: store screenshots under data

--- Cheerio/my-crawler/src/main.js
+++ Cheerio/my-crawler/src/main.js
@@ -3,48 +3,34 @@ import { PlaywrightCrawler } from 'crawlee';
 import { fileURLToPath } from 'url';
 import { dirname, resolve } from 'path';
 import fs from 'fs';
+import { convertToBase64 } from './logicScreenshots/convertImageToBase64.js';
 
-// Ermittelt den Pfad zur aktuellen Datei
 const __filename = fileURLToPath(import.meta.url);
-import {convertToBase64} from './logicScreenshots/convertImageToBase64.js'
 const __dirname = dirname(__filename);
-var imageBase64;
+let imageBase64;
 
-const screenshotsDir = resolve(__dirname, 'screenshots');
-    if (!fs.existsSync(screenshotsDir)) {
-        fs.mkdirSync(screenshotsDir);
-    }
-// PlaywrightCrawler crawls the web using a headless
-// browser controlled by the Playwright library.
+const screenshotsDir = resolve(__dirname, '../../data/screenshots');
+if (!fs.existsSync(screenshotsDir)) {
+    fs.mkdirSync(screenshotsDir, { recursive: true });
+}
+
 const crawler = new PlaywrightCrawler({
-    // Use the requestHandler to process each of the crawled pages.
     async requestHandler({ request, page, enqueueLinks, log, pushData }) {
         const title = await page.title();
         log.info(`Title of ${request.loadedUrl} is '${title}'`);
         const screenshotPath = resolve(screenshotsDir, `${new Date().toISOString()}_screenshot.png`);
-        await page.screenshot({ path: screenshotPath, fullPage: true, width: 1024, height: 1024});
+        await page.screenshot({ path: screenshotPath, fullPage: true, width: 1024, height: 1024 });
         log.info(`Screenshot saved to ${screenshotPath}`);
         imageBase64 = convertToBase64(screenshotPath);
 
-        // Save results as JSON to ./storage/datasets/default
         await pushData({ title, url: request.loadedUrl });
-
-        // Extract links from the current page
-        // and add them to the crawling queue.
         await enqueueLinks();
     },
-    // Comment this option to scrape the full website.
     maxRequestsPerCrawl: 1,
-    // Uncomment this option to see the browser window.
-    // headless: false,
 });
 
-// Add first URL to the queue and start the crawl.
 export async function startCrawler(url){
-
-await crawler.run([url]);
-
-console.log("IN CRAWLER");
-
-return imageBase64;
+    await crawler.run([url]);
+    console.log('IN CRAWLER');
+    return imageBase64;
 }
